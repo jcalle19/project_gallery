f06dd17506ae618873db927015c6ee289d36cc41
jcalle19 <jcalle19@asu.edu>
2025-11-21 17:47:39 -0700

    Starting app architecture
    Setup boilerplate files and folders while getting rid of some default files

---
+++ express-server/server.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
+import cors from 'cors';
+//import socket_handler from './socket_handler.js';
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: '*',
+        methods: ['GET', 'POST'],
+    },
+});
+
+const PORT = 5000;
+
+// Middleware
+app.use(cors());
+
+// Socket handlers (if exported as default)
+//const socket_functions = socket_handler;
+//socket_functions(io);
+
+server.listen(PORT, () => {
+    console.log(`Server is listening at http://localhost:${PORT}`);
+});
