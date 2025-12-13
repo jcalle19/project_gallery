6a210588f193d2e480fd840bc13cd1acb4748d91
jcalle19 <jcalle19@asu.edu>
2025-12-12 22:32:52 -0700

    Added DB integration
    Added DB connection to server file

--- express-server/server.js
+++ express-server/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import http from 'http';
 import { Server } from 'socket.io';
 import { socket_functions } from './socket_handler.js'
+import { DB_connect } from './database.js'
 import cors from 'cors';
 //import socket_handler from './socket_handler.js';
 
@@ -19,8 +20,11 @@ const PORT = 5000;
 // Middleware
 app.use(cors());
 
+//Database
+DB_connect(io);
+
 //Socket Events
-socket_functions(io);
+//socket_functions(io);
 
 server.listen(PORT, () => {
     console.log(`Server is listening at http://localhost:${PORT}`);
